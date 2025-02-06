1945c1915df90493080aeb54a735ccf2d72a9630
YehorSamofal <27samofy@retsdstudent.ca>
2025-02-06 20:15:36 +0000

    lol

--- src/assignment.py
+++ src/assignment.py
@@ -1,10 +1,14 @@
 # Arrange these print statements to spell out a secret word!
 # Don't change the print statements, just move them around.
-
 def spell_word():
-    print("N") 
-    print("H")
-    print("Y")
     print("P")
+    print("Y")
     print("T")
+    print("H")
     print("O")
+    print("N") 
+    
+    
+    
+    
+    
